b0d7d5448ecb5ef538238bd0ec46ffc7214cfaf1
RobinDeSmet <robin_de_smet@hotmail.be>
2024-06-08 17:04:27 +0200

    feat: add str overloader for SentimentType

--- src/custom_types.py
+++ src/custom_types.py
@@ -6,3 +6,15 @@ class SentimentType(Enum):
     NEGATIVE = -1
     NEUTRAL = 0
     NOT_UNDERSTOOD = 2
+
+    def __str__(self):
+        if self == SentimentType.POSITIVE:
+            return "Positive"
+        elif self == SentimentType.NEGATIVE:
+            return "Negative"
+        elif self == SentimentType.NEUTRAL:
+            return "Neutral"
+        elif self == SentimentType.NOT_UNDERSTOOD:
+            return "Not Understood"
+        else:
+            return super().__str__()
